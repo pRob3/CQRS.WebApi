bd5ff60ac26031b8d8b99b180648cb1657fd901c
pRob3 <hundraprocent@me.com>
2021-05-28 15:29:32 +0200

    Add project files.

---
+++ CQRS.WebApi/Context/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using CQRS.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS.WebApi.Context
+{
+    public class ApplicationContext : DbContext, IApplicationContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+
+        }
+
+        public async Task<int> SaveChanges()
+        {
+            return await base.SaveChangesAsync();
+        }
+    }
+}
